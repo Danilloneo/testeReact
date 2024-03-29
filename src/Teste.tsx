d926bee35ed1c4f69896983c2d4719ab3f571ce5
Danillo Guimarães <151641788+Danilloneo@users.noreply.github.com>
2024-03-28 23:14:27 -0300

    Finalizando a aula e a instalação do styled components

--- src/Teste.tsx
+++ src/Teste.tsx
@@ -9,16 +9,27 @@ const Botao = styled.button<BotaoProps>`
   background-color: ${(props) => (props.principal ? 'green' : 'blue')};
   font-size: ${(props) => props.fontSize || '16px'};
 `
+const BotaoPerigo = styled(Botao)`
+  background-color: red;
+  color: #fff;
+
+  span {
+    text-decoration: line-through;
+  }
+`
 
 function Teste() {
   return (
     <>
-      <Botao fontSize="18px" principal>
-        Clique·aqui!
-      </Botao>
+      <Botao principal>Clique·aqui!</Botao>
       <Botao fontSize="14px" principal={false}>
         Cancelar
       </Botao>
+      <BotaoPerigo as="a" principal>
+        <span>Não clique aqui</span>
+      </BotaoPerigo>
+      {/* {principal ?: boolean } */}
+      {/* <BotaoPerigo>Não clique aqui</BotaoPerigo> */}
     </>
   )
 }
